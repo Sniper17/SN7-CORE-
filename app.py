38c565b7fafaa4e4c00234618ab276cc1cf2d7c9
Sniper17 <Sniper17@users.noreply.github.com>
2026-08-24 11:06:38 -0300

    Add legal links to homepage

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.config.update(
     SESSION_COOKIE_SAMESITE="Lax",
 )
 
-SN7_VERSION = "1.9.28"
+SN7_VERSION = "1.9.29"
 SN7_STATIC_CACHE = "public, max-age=31536000, immutable"
 
 app.register_blueprint(economy_bp, url_prefix="/api/economy")
